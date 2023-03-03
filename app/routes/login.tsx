476f436ef3b6ca7f2a1b19172ee05711d1cd5a8a
D'Angela Anderson <ande1940@msu.edu>
2023-03-03 07:25:21 -0500

    Created The Log In & Sign Up Route

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -3,19 +3,23 @@ import { Link } from "react-router-dom"
 
 export default function LogIn() {
   return (
-    <div>
-      <Form method="post">
-        Dont have an account? <Link to="/signup">Register here</Link>
-        <div>
+    <div className="flex flex-col w-screen h-screen justify-center items-center">
+      <Form className="grid space-y-4" method="post">
+      <div className="flex mb-8 place-content-center">Log In</div>
+        <div className="flex space-x-2">
+          <div>Dont have an account? </div>
+          <Link to="/signup">Register here</Link>
+        </div>
+        <div className="grid space-y-2">
           <label htmlFor="email">Email Address</label>
-          <input type="email" name="email" id="email" required />
+          <input className="px-3 h-10 rounded-md border border-slate-300" type="email" name="email" id="email" required />
         </div>
-        <div>
+        <div className="grid space-y-2">
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" required/>
+          <input className="px-3 h-10 rounded-md border border-slate-300" type="password" name="password" id="password" required/>
         </div>
-        <div>
-          <button type="submit" name="submit" value="intent">Log In</button>
+        <div className="grid space-y-2">
+          <button className="h-10 text-white bg-black" type="submit" name="submit" value="intent">Log In</button>
         </div>
       </Form>
     </div>
--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -2,28 +2,31 @@ import { Form } from "@remix-run/react";
 
 export default function SignUp() {
     return (
-      <div>
-        <Form>
-          <div className="flex">
-          <div>
-            <label htmlFor="firstName">First Name</label>
-            <input type="firstName" name="firstName" id="firstName" required />
+      <div className="flex flex-col w-screen h-screen justify-center items-center">
+        <Form className="grid space-y-4" method="post">
+          <div className="flex mb-8 place-content-center">
+            Sign up
           </div>
-          <div>
-            <label htmlFor="lastName">LastName</label>
-            <input type="lastName" name="lastName" id="lastName" required/>
-          </div>  
+          <div className="flex space-x-4">
+            <div className="grid space-y-2">
+              <label htmlFor="firstName">First Name</label>
+              <input className="px-3 h-10 rounded-md border border-slate-300" type="firstName" name="firstName" id="firstName" required />
+            </div>
+            <div className="grid space-y-2">
+              <label htmlFor="lastName">Last Name</label>
+              <input className="px-3 h-10 rounded-md border border-slate-300" type="lastName" name="lastName" id="lastName" required/>
+            </div>  
           </div>
-          <div>
+          <div className="grid space-y-2 space-y-2">
             <label htmlFor="email">Email Address</label>
-            <input type="email" name="email" id="email" required />
+            <input className="px-3 h-10 rounded-md border border-slate-300" type="email" name="email" id="email" required />
           </div>
-          <div>
+          <div className="grid space-y-2">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" required/>
+            <input className="px-3 h-10 rounded-md border border-slate-300" type="password" name="password" id="password" required/>
           </div>
-          <div>
-            <button type="submit" name="submit" value="intent">Log In</button>
+          <div className="grid">
+            <button className="h-10 text-white bg-black" type="submit" name="submit" value="intent">Log In</button>
           </div>
         </Form>
       </div>
